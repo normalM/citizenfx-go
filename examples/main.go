37be27960d3f10eddfdf329b315968a58bd8b691
Kittizz <62899732+kittizz@users.noreply.github.com>
2021-10-08 05:37:10 +0700

    Update main.go

--- examples/main.go
+++ examples/main.go
@@ -7,26 +7,27 @@ import (
 	cfx "github.com/normalM/citizenfx-go"
 )
 
-var c = make(chan bool)
+var (
+	c  = make(chan bool)
+	fn = js.FuncOf
+)
 
 func init() {
 	cfx.Print(fmt.Sprintf("wasm %s loaded.", cfx.Server.GetCurrentResourceName()))
 }
 func main() {
 
-	cfx.Server.RegisterCommand("asd", js.FuncOf(funcx), false)
+	cfx.Server.RegisterCommand("asd", fn(func(this js.Value, inputs []js.Value) interface{} {
+
+		cfx.TriggerClientEvent(
+			"chat:addMessage",
+			-1,
+			"TEST!!",
+		)
+		cfx.Print("DO IT!!!!!!!")
+
+		return nil
+	}), false)
 
 	<-c
-}
-
-func funcx(this js.Value, inputs []js.Value) interface{} {
-
-	cfx.TriggerClientEvent(
-		"chat:addMessage",
-		-1,
-		"TEST!!",
-	)
-	cfx.Print("DO IT!!!!!!!")
-
-	return nil
 }
